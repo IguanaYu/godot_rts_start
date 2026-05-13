b6548d2194d5b3de288db4ed276003282437c49d
IguanaYu <iguana4623@qq.com>
2026-05-13 15:30:39 +0800

    修复治疗特效帧数错误：裁剪图片为5帧+动态计算hframes（已测试）

--- scripts/heal_effect.gd
+++ scripts/heal_effect.gd
@@ -3,9 +3,16 @@ extends Node2D
 @onready var sprite: Sprite2D = $Sprite
 
 var _frame: int = 0
-var _total_frames: int = 6
+var _total_frames: int = 5
 var _fps: float = 14.0
 var _timer: float = 0.0
+
+func _ready() -> void:
+	var tex: Texture2D = sprite.texture
+	if tex:
+		_total_frames = tex.get_width() / 192
+		sprite.hframes = _total_frames
+		sprite.frame = 0
 
 func _process(delta: float) -> void:
 	_timer += delta
